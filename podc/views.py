d5ae6fc5181bf4e8ac272912d6eb0689c675e385
Blaise Laflamme <blaise@laflamme.org>
2013-01-03 22:51:09 -0500

    updated project title

--- podc/views.py
+++ podc/views.py
@@ -3,4 +3,4 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
-    return {'project': 'podc'}
+    return {'project': 'Pyramid On DotCloud'}
